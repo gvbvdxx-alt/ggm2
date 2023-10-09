6f5dc7a4c573ee66799092ade4c71cfaa5a144db
gvbvdxx-test <gvbvdxx@gmail.com>
2023-10-09 07:21:47 -0500

    Upload source code

--- upload-website.sh
+++ upload-website.sh
@@ -2,5 +2,5 @@ git init
 git add ./*
 git commit -m "Upload source code"
 git branch -M website
-git remote add origin https://github.com/gvbvdxx-test/ggm2.git
+git remote add origin https://github.com/gvbvdxx-alt/ggm2.git
 git push -f origin website
